b6a842682ab20889048e7a910911c1b249162d82
anitiw <27tiwari372@sasd.net>
2026-01-20 20:54:25 -0600

    adding a command for shooting out fuel.

---
+++ src/main/java/frc/robot/Commands/ShootsOut.java
@@ -0,0 +1,75 @@
+package frc.robot.Commands;
+
+import com.pathplanner.lib.path.PathConstraints;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants.ConstantValues;
+import frc.robot.Constants.FieldConstants;
+import frc.robot.Constants.PhysicalConstants;
+import frc.robot.subsystems.Secondary.Indexer;
+import frc.robot.subsystems.Secondary.Outtake;
+import frc.robot.subsystems.drive.CommandSwerveDrivetrain;
+
+public class ShootsOut extends Command {
+
+    private Indexer m_indexer;
+    private Outtake m_outtake;
+    private CommandSwerveDrivetrain drivetrain;
+    private double distance;
+    private double lowerBoundAngle;
+    private double upperBoundAngle;
+    private double angle;
+    private double yaw;
+
+    public ShootsOut(Outtake m_outtake, Indexer m_indexer, CommandSwerveDrivetrain drivetrain) {
+        this.m_indexer = m_indexer;
+        this.m_outtake = m_outtake;
+        this.drivetrain = drivetrain;
+    }
+
+    @Override
+    public void initialize(){
+    
+    m_outtake.setVelocity(ConstantValues.SHOOTER_RPM);                    
+
+    }
+
+    @Override
+    public void execute(){
+        distance =  Math.sqrt(Math.pow(182.1 - drivetrain.getState().Pose.getX(), 2) + Math.pow(158.5 - drivetrain.getState().Pose.getY(), 2));
+        lowerBoundAngle = Math.atan((Math.pow(ConstantValues.SHOOTER_SPEED, 2) + 
+                          Math.sqrt(Math.pow(ConstantValues.SHOOTER_SPEED,4) - 
+                          384 * (384 * Math.pow(distance - FieldConstants.SMALLEST_RADIUS_OF_HOLE, 2) + 
+                          2 * (FieldConstants.HEIGHT_OF_HOLE - PhysicalConstants.SHOOTER_HEIGHT) * 
+                          Math.pow(ConstantValues.SHOOTER_SPEED, 2)))) / 
+                          384 * (distance - FieldConstants.SMALLEST_RADIUS_OF_HOLE));
+    
+
+                          
+        upperBoundAngle = Math.atan((Math.pow(ConstantValues.SHOOTER_SPEED, 2) + 
+                          Math.sqrt(Math.pow(ConstantValues.SHOOTER_SPEED,4) - 
+                          384 * (384 * Math.pow(distance + FieldConstants.SMALLEST_RADIUS_OF_HOLE, 2) + 
+                          2 * (FieldConstants.HEIGHT_OF_HOLE - PhysicalConstants.SHOOTER_HEIGHT) * 
+                          Math.pow(ConstantValues.SHOOTER_SPEED, 2)))) / 
+                          384 * (distance + FieldConstants.SMALLEST_RADIUS_OF_HOLE));
+    
+        angle = (lowerBoundAngle + upperBoundAngle) / 2;
+        
+        yaw = Math.atan((158.5 - drivetrain.getState().Pose.getY())/(182.1 - drivetrain.getState().Pose.getX()));
+        // m_turret.setTurretCmd(yaw);
+        //if(Math.abs(m_rotate.armAngMtr.getPosition().getValueAsDouble() - angle) <= 1 && Math.abs(m_outtake.wheelSpeedMtr.getVelocity().getValueAsDouble() - ConstantValues.SHOOTER_RPM) <= 30 && Math.abs(m_turret.turretAngMtr.getPosition().getValueAsDouble() - yaw) <= 1){
+            if(Math.abs(m_outtake.wheelSpeedMtr.getVelocity().getValueAsDouble() - ConstantValues.SHOOTER_RPM) <= 30 && Math.abs(drivetrain.getState().Pose.getRotation().getRadians() - yaw) <= 0.5){
+            m_indexer.indexMtrLdr.setVoltage(10);;
+        } else {
+            m_indexer.indexMtrLdr.setVoltage(0);
+        }
+    }
+
+    @Override
+    public void end(boolean interrupted){
+        m_outtake.setVelocity(0);
+        m_indexer.indexMtrLdr.set(0);
+    }
+}
